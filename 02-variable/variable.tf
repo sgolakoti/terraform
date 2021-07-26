70468497107bee62d4f98729dfd0546baee6e411
Sankar <sankargolakoti@gmail.com>
2021-07-26 21:11:58 +0530

    var2

--- 02-variable/variable.tf
+++ 02-variable/variable.tf
@@ -2,5 +2,11 @@ variable "var1" {
   default="Hello World"
 }
 output "varout" {
-  value ="My first test is var.${var.var1}"
+  value ="My first test is ${var.var1}"
+}
+
+variable "var2" {}
+
+output "out2" {
+  value = var.var2
 }
