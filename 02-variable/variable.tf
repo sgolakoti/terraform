eb64dc6b5d10273826af6821811a79708daa2d21
Sankar <sankargolakoti@gmail.com>
2021-07-26 21:22:46 +0530

    varlilst

--- 02-variable/variable.tf
+++ 02-variable/variable.tf
@@ -10,3 +10,24 @@ variable "var2" {}
 output "out2" {
   value = var.var2
 }
+
+variable "varlist" {
+  default=["1stval","2ndval","3rdval"]
+}
+
+variable "varmap" {
+  default = {
+    aws = "7.30 AM"
+    office = "9.00 AM"
+    practice = "6.00 PM"
+  }
+}
+output "out3" {
+  value = var.varlist
+}
+output "out4" {
+  value = "My list is 1. ${var.varlist[0]} 2. ${var.varlist[1]} 3. ${var.varlist[2]}"
+}
+output "out5" {
+  value = "My list is 1. ${var.varmap[aws]} 2. ${var.varmap[office]} 3. ${var.varmap[practice]}"
+}
